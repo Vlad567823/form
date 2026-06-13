82d4f81ace02ad288e467102d72cc1fd84b6d59d
Vlad567823 <165555735+Vlad567823@users.noreply.github.com>
2026-06-13 21:42:16 +0300

    form

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,17 @@
+export default function ContactItem({
+  contact,
+  onDelete,
+}) {
+  return (
+    <li>
+      {contact.name}: {contact.number}
+
+      <button
+        type="button"
+        onClick={() => onDelete(contact.id)}
+      >
+        Delete
+      </button>
+    </li>
+  );
+}
